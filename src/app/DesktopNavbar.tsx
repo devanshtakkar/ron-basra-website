2a2645634ee80251bd8de1a2c8562af03d92e27e
Devansh Takkar <devanshtakkar@gmail.com>
2025-02-17 20:36:19 -0800

    added desktop navbar

---
+++ src/app/DesktopNavbar.tsx
@@ -0,0 +1,172 @@
+"use client";
+import { 
+  Box, 
+  Container, 
+  Stack, 
+  Menu,
+  MenuItem,
+  Button,
+  Typography
+} from "@mui/material";
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+export default function DesktopNavbar() {
+  const [anchorEls, setAnchorEls] = useState<{ [key: string]: HTMLElement | null }>({});
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>, menuId: string) => {
+    setAnchorEls({ ...anchorEls, [menuId]: event.currentTarget });
+  };
+
+  const handleClose = (menuId: string) => {
+    setAnchorEls({ ...anchorEls, [menuId]: null });
+  };
+
+  const menuItems = [
+    { title: "Home", path: "/" },
+    {
+      title: "Properties",
+      id: "properties",
+      children: [
+        { title: "My Listings", path: "/properties/my-listings" },
+        { title: "Office Listings", path: "/properties/office-listings" }
+      ]
+    },
+    {
+      title: "Buying",
+      id: "buying",
+      children: [
+        { title: "Buying Info", path: "/buying/info" },
+        { title: "Mortgage Calculator", path: "/buying/mortgage-calculator" },
+        { title: "Reports", path: "/buying/reports" }
+      ]
+    },
+    {
+      title: "Selling",
+      id: "selling",
+      children: [
+        { title: "Selling Info", path: "/selling/info" },
+        { title: "Reports", path: "/selling/reports" }
+      ]
+    },
+    { title: "Market Reports", path: "/market-reports" },
+    { title: "Blogs", path: "/blogs" },
+    { title: "Contact", path: "/contact" }
+  ];
+
+  return (
+    <Box
+      height={70}
+      sx={{
+        backgroundColor: "#232938",
+        borderBottom: "1px solid #e0e0e0",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          height="100%"
+        >
+          <Box
+            sx={{
+              position: "relative",
+              width: "200px",
+              height: 60,
+            }}
+          >
+            <Link href="/">
+              <Image
+                src="/images/Ron Basra Logo Gold.png"
+                alt="Ron Basra"
+                fill
+                style={{
+                  objectFit: "contain",
+                }}
+              />
+            </Link>
+          </Box>
+
+          <Stack direction="row" spacing={2} alignItems="center">
+            {menuItems.map((item) => (
+              <Box key={item.title}>
+                {item.children ? (
+                  <>
+                    <Button
+                      onClick={(e) => handleClick(e, item.id)}
+                      endIcon={<KeyboardArrowDownIcon />}
+                      sx={{ 
+                        color: 'white',
+                        textTransform: 'none',
+                        '&:hover': {
+                          backgroundColor: 'transparent',
+                          color: '#D4AF37'
+                        }
+                      }}
+                    >
+                      {item.title}
+                    </Button>
+                    <Menu
+                      anchorEl={anchorEls[item.id]}
+                      open={Boolean(anchorEls[item.id])}
+                      onClose={() => handleClose(item.id)}
+                      MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                      }}
+                      sx={{
+                        '& .MuiPaper-root': {
+                          borderRadius: 2,
+                          marginTop: 1,
+                          minWidth: 180,
+                        }
+                      }}
+                    >
+                      {item.children.map((child) => (
+                        <MenuItem 
+                          key={child.title}
+                          component={Link}
+                          href={child.path}
+                          onClick={() => handleClose(item.id)}
+                          sx={{
+                            color: 'black',
+                            '&:hover': {
+                              color: '#D4AF37',
+                              backgroundColor: 'transparent'
+                            }
+                          }}
+                        >
+                          {child.title}
+                        </MenuItem>
+                      ))}
+                    </Menu>
+                  </>
+                ) : (
+                  <Link 
+                    href={item.path}
+                    style={{ 
+                      textDecoration: 'none',
+                      color: 'white'
+                    }}
+                  >
+                    <Typography 
+                      sx={{
+                        '&:hover': {
+                          color: '#D4AF37'
+                        }
+                      }}
+                    >
+                      {item.title}
+                    </Typography>
+                  </Link>
+                )}
+              </Box>
+            ))}
+          </Stack>
+        </Stack>
+      </Container>
+    </Box>
+  );
+} 
--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,10 +1,15 @@
 import MobileNavbar from "./MobileNavbar";
+import DesktopNavbar from "./DesktopNavbar";
 import Box from "@mui/material/Box";
-export default function Navbar(){
-    return(
+
+export default function Navbar() {
+    return (
         <nav>
-            <Box sx={{display: {xs: "block", md: "none"}}}>
+            <Box sx={{ display: { xs: "block", md: "none" } }}>
                 <MobileNavbar />
+            </Box>
+            <Box sx={{ display: { xs: "none", md: "block" } }}>
+                <DesktopNavbar />
             </Box>
         </nav>
     )
